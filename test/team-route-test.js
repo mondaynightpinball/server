82b641b77bb037a7e16410d333b80b4649ccca15
Geoff Simons <geoff.simons@gmail.com>
2017-03-05 23:19:48 -0800

    Multi mock-user

--- test/team-route-test.js
+++ test/team-route-test.js
@@ -19,6 +19,27 @@ const exampleTeam = {
   code: 'exm'
 };
 
+function mockUsers(fieldnames) {
+  debug('mockUsers:',fieldnames);
+  let list = fieldnames;
+  let users = this;
+
+  return new Promise( (resolve, reject) => {
+    function _make() {
+      if(list.length === 0) return resolve(); //TODO: resolve what?
+      let fn = list.pop();
+      require('./lib/mock-user.js')()
+      .then( user => {
+        debug(fn,user);
+        users[fn] = user;
+        _make();
+      })
+      .catch(reject);
+    }
+    _make();
+  });
+}
+
 describe('Team Routes', function() {
   before( done => serverToggle.start(server, done));
   before( done => { // CREATE A MOCK ROOT USER
@@ -28,6 +49,20 @@ describe('Team Routes', function() {
       done();
     });
   });
+
+  before( done => {
+    mockUsers.call(this, [
+      'captain','cocap','player1','player2','player3','player4','rando'
+    ])
+    .then(done)
+    // .then( () => {
+    //   debug('captain:',this.captain);
+    //   done();
+    // })
+    .catch(done);
+  });
+
+/*
   before( done => {
     require('./lib/mock-user.js')()
     .then( user => {
@@ -35,6 +70,21 @@ describe('Team Routes', function() {
       done();
     });
   });
+  before( done => {
+    require('./lib/mock-user.js')()
+    .then( user => {
+      this.captain = user;
+      done();
+    });
+  });
+  before( done => {
+    require('./lib/mock-user.js')()
+    .then( user => {
+      this.player = user;
+      done();
+    });
+  });
+*/
   after(require('./lib/clean-db.js'));
   after( done => serverToggle.stop(server, done));
 
@@ -81,23 +131,23 @@ describe('Team Routes', function() {
   }); // GET /api/team/:id
 
   describe('PUT /api/team/:id/player', () => {
-    describe('with valid auth and valid player', () => {
-      it('should add a player to the team', done => {
+    describe('with valid auth and valid captain', () => {
+      it('should add a captain to the team', done => {
         request.put(`${url}/api/team/${this.team._id}/player`)
         .set({ Authorization: `Bearer ${this.root.token}`})
-        .send({ playerId: this.rando._id, role: 'player' })
+        .send({ playerId: this.captain._id, role: 'captain' })
         .end( (err, res) => {
           debug(res.body);
           expect(res.status).to.equal(202);
           expect(res.body.roster.length).to.equal(1);
-          expect(res.body.roster[0]).to.equal(this.rando._id.toString());
+          expect(res.body.roster[0]).to.equal(this.captain._id.toString());
           done();
         });
-      });
+      }); // rando
     }); // valid request
 
     //TODO: Test for roles
     //TODO: Test for redundant add
-    
+
   }); // PUT /api/team/:id/player
 });
